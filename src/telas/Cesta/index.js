5dba0f932600e17d46bfdd2864a3de7128c0f8ae
andregtalima <andregtalima@gmail.com>
2023-09-27 21:40:34 -0300

    Finalização Orgs Cesta

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet } from 'react-native';
 
 import Topo from './components/Topo';
 import Detalhes from './components/Detalhes';
-import Itens from './components/Itens';
+import Item from './components/Item';
+import Font from '../../components/Font';
 
 export default function Cesta({ topo, detalhes, itens }) {
     return (
-        <ScrollView> 
-
-            <Topo {...topo} />
-            <View style={styles.cesta}>
-                <Detalhes {...detalhes}/>    
-                <Itens {...itens}/> 
-            </View>  
-              
-        </ScrollView>  
+        <> 
+            <FlatList
+                data={itens.list}
+                renderItem={Item}
+                keyExtractor={({name}) => name}
+                ListHeaderComponent={() => {
+                    return (
+                        <>
+                            <Topo {...topo} />
+                            <View style={styles.cesta}>
+                                <Detalhes {...detalhes}/> 
+                                <Font style={styles.title}>{itens.title}</Font>   
+                            </View> 
+                        </> 
+                    )
+                }}
+            />              
+        </>  
     )
 }
 
 const styles = StyleSheet.create({
+    title: {
+        color: '#464646',
+        fontWeight: 'bold',
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20, 
+        lineHeight: 32
+    },
     cesta: {
         paddingVertical: 8,
         paddingHorizontal: 16,
